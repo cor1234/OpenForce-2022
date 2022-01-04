ed4e60e4b1e4f30766ffed22b6e9e5392dbca93c
Rohan Kumar <95542126+cor1234@users.noreply.github.com>
2022-01-04 17:46:31 +0530

    Add files via upload

--- footer.js
+++ footer.js
@@ -1,59 +1,65 @@
-import styled from 'styled-components';
+import React from 'react'
+import Footer from '../components/footer'
+import Icon from '../components/icons'
 
-export const Container = styled.div`
-  padding: 80px 60px;
-  ${'' /* background: radial-gradient(circle, rgba(92,39,251,1) 0%, rgba(112,71,247,1) 100%); */}
+export function FooterContainer() {
+    return (
+        <>
+        <h1 style={{ color: "white", 
+                   textAlign: "center", 
+                   marginTop: "0px",
+                   textDecorationLine:"underline"}}>
+        Made By CodeFlow
+      </h1>
+        <Footer>
+            <Footer.Wrapper>
+            <Footer.Row>
+            <Footer.Column>
+            <Footer.Link href="#"><img src="./openforce Logo 1 (1).jpg" alt="" style={{}}></img></Footer.Link>
+                <Footer.Title>Social Media </Footer.Title>
 
-  @media (max-width: 1000px) {
-    padding: 70px 30px;
-  }
-`;
-
-export const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    max-width: 1000px;
-    margin: 0 auto;
-    /* background: red; */
-`
-
-export const Column = styled.div`
-  display: flex;
-  flex-direction: column;
-  text-align: left;
-  margin-left: 60px;
-  
-`;
-
-export const Row = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
-  grid-gap: 20px;
-
-  @media (max-width: 1000px) {
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  }
-`;
-
-export const Link = styled.a`
-  color: #fff;
-  margin-bottom: 20px;
-  font-size: 18px;
-  text-decoration:'none';
-  
-
-  &:hover {
-      color: #ff9c00;
-      transition: 200ms ease-in;
-  }
-`;
-
-export const Title = styled.p`
-  font-size: 24px;
-  color: #fff;
-  margin-bottom: 40px;
-  font-weight: bold;
-   text-decoration:'underline';
-`;
-
+                
+                   
+                   
+                    {/* <Footer.Link href="#"><Icon className="fab fa-instagram" /></Footer.Link>
+                    <Footer.Link href="#"><Icon className="fab fa-youtube" /></Footer.Link>
+                    <Footer.Link href="#"><Icon className="fab fa-twitter" /></Footer.Link> */}
+                   
+                    <Footer.Link href="#"><Icon className="fab fa-facebook-f" /><Icon className="fab fa-instagram" /><Icon className="fab fa-youtube" /><Icon className="fab fa-twitter" /></Footer.Link>
+                    {/* <a href="#"><Icon className="fab fa-facebook-f" /></a>
+                    <a href="#"><Icon className="fab fa-instagram" /></a>
+                    <a href="#"><Icon className="fab fa-youtube" /></a>
+                    <a href="#"><Icon className="fab fa-twitter" /></a> */}
+                </Footer.Column>
+                <Footer.Column> 
+                 <Footer.Title>Company</Footer.Title>
+                    <Footer.Link href="#">About us</Footer.Link>
+                    <Footer.Link href="#">Our Services</Footer.Link>
+                    <Footer.Link href="#">Privacy Policy</Footer.Link>
+                    <Footer.Link href="#">Affiliated Program</Footer.Link>
+                </Footer.Column>
+                <Footer.Column>
+                <Footer.Title>Get Help</Footer.Title>
+                    <Footer.Link href="#">FAQ</Footer.Link>
+                    <Footer.Link href="#">Codeflow</Footer.Link>
+                    <Footer.Link href="#">Message</Footer.Link>
+                    <Footer.Link href="#">Payment Detail</Footer.Link>
+                </Footer.Column>
+                <Footer.Column>
+                <Footer.Title>Prize</Footer.Title>
+                    <Footer.Link href="#">Watch</Footer.Link>
+                    <Footer.Link href="#">Bagpack</Footer.Link>
+                    <Footer.Link href="#">Shoes</Footer.Link>
+                    <Footer.Link href="#">Dress</Footer.Link>
+                </Footer.Column>
+               
+                
+            
+            </Footer.Row>
+            </Footer.Wrapper>
+            
+        </Footer>
+        
+        </>
+    ) 
+}
